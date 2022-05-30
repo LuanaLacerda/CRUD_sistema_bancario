9c34bada044bde37f8f81dba6e503df24d5648ef
Luana Lacerda <luanaretifica@hotmail.com>
2022-05-30 09:45:58 -0300

    Atualização deploy na heroku

--- src/conexao.js
+++ src/conexao.js
@@ -3,7 +3,7 @@ const knex = require('knex')({
     connection: {
         host: 'ec2-34-231-221-151.compute-1.amazonaws.com',
         user: 'lnheguijquumss',
-        password: '270808',
+        password: 'c8077da8da6e0b274feeeba556b87e9febaa47419f4d320893e1fcb968f74872',
         database: 'd2v83rmk915clg',
         port: 5432,
         ssl: {
