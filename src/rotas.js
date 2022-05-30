cad61c56d6fe7a0fa5b6c028c82ceffc442dfbf9
Luana Lacerda <luanaretifica@hotmail.com>
2022-05-30 08:57:25 -0300

    Rotas atualizadas

--- src/rotas.js
+++ src/rotas.js
@@ -3,6 +3,7 @@ const contas = require('./controladores/contas');
 const rotas = express();
 const login = require('./controladores/login');
 const verificaLogin = require('./filtros/verificarLogin')
+const transações = require('./controladores/transacoes')
 
 //Cadastro conta
 rotas.post('/contas', contas.cadastrarContas);
@@ -18,5 +19,10 @@ rotas.get('/usuario', contas.listarDadosUsuarioLogado);
 rotas.put('/usuario', contas.atualizarUsuario);
 
 //transações
+
+rotas.post('/deposito', transações.deposito);
+rotas.post('/saque', transações.saque);
+rotas.get('/saldo', transações.obterSaldo);
+rotas.get('/transacoes', transações.transacoesRealizadas);
 
 module.exports = rotas;
