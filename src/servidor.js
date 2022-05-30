1bc00810fc3f38841385cd3b9e03ca29ccea88c9
Luana Lacerda <luanaretifica@hotmail.com>
2022-05-30 10:55:14 -0300

    Atualziação da documentação API

--- src/servidor.js
+++ src/servidor.js
@@ -2,14 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const rotas = require('./rotas');
 const app = express();
-
-
+const cors = require('cors')
 
 
 app.use(express.json());
+
 
 app.use(express.json());
 app.use(rotas);
+app.use(cors());
 
 
 module.exports = app
